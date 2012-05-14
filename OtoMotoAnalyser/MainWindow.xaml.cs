ccabfdc57b401dbe46bcd3b3330f6e397f2d126b
mateuszszulc <mateusz.szulc@gmail.com>
2012-05-14 17:53:51 +0200

    ---

--- OtoMotoAnalyser/MainWindow.xaml.cs
+++ OtoMotoAnalyser/MainWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using HtmlAgilityPack;
 
 namespace OtoMotoAnalyser
 {
@@ -22,6 +23,56 @@ namespace OtoMotoAnalyser
         public MainWindow()
         {
             InitializeComponent();
+            string filePath = "otomoto.txt";
+            HtmlDocument htmlDoc = new HtmlDocument();
+
+            // There are various options, set as needed
+            htmlDoc.OptionFixNestedTags = true;
+
+            // filePath is a path to a file containing the html
+            htmlDoc.Load(filePath);
+            //htmlDoc.LoadHtml(@"<html><body><p><table id=""foo""><tr><th>hello</th></tr><tr><td>world</td></tr></table></body></html>");
+            // Use:  htmlDoc.LoadXML(xmlString);  to load from a string
+            HtmlNode node = htmlDoc.GetElementbyId("mainRightOM");
+            Console.WriteLine(node.InnerHtml);
+
+            // ParseErrors is an ArrayList containing any errors from the Load statement
+            //if (htmlDoc.ParseErrors != null && htmlDoc.ParseErrors.Count > 0)
+            if (htmlDoc.ParseErrors != null)
+            {
+                // Handle any parse errors as required
+
+            }
+            else
+            {
+
+                if (htmlDoc.DocumentNode != null)
+                {
+                    HtmlNode bodyNode = htmlDoc.DocumentNode.SelectSingleNode("//body");
+
+                    if (bodyNode != null)
+                    {
+                        // Do something with bodyNode
+                    }
+                }
+            }
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.LoadHtml(@"<html><body><p><table id=""foo""><tr><th>hello</th></tr><tr><td>world</td></tr></table></body></html>");
+            foreach (HtmlNode table in doc.DocumentNode.SelectNodes("//table"))
+            {
+                Console.WriteLine("Found: " + table.Id);
+                foreach (HtmlNode row in table.SelectNodes("tr"))
+                {
+                    Console.WriteLine("row");
+                    foreach (HtmlNode cell in row.SelectNodes("th|td"))
+                    {
+                        Console.WriteLine("cell: " + cell.InnerText);
+                    }
+                }
+            }
+
+
         }
     }
 }
